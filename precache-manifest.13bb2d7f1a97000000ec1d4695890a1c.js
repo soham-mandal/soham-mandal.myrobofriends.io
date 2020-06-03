ce67c2aa77445e76b643f3ee735c9aa3965f13ca
soham-mandal <soham99mandal@gmail.com>
2020-06-03 23:44:27 +0530

    upload

---
+++ precache-manifest.13bb2d7f1a97000000ec1d4695890a1c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a16334ee0f8155481380a3892994c555",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af19bb4f97c2051a7046",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e951772e42bcefb048cc",
+    "url": "/static/css/main.1b61f86d.chunk.css"
+  },
+  {
+    "revision": "af19bb4f97c2051a7046",
+    "url": "/static/js/2.61e5aa5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.61e5aa5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e951772e42bcefb048cc",
+    "url": "/static/js/main.b8151099.chunk.js"
+  },
+  {
+    "revision": "5ef23210415a69b87246",
+    "url": "/static/js/runtime-main.6dda8a00.js"
+  },
+  {
+    "revision": "0fd919996af422c8394f539826bb8904",
+    "url": "/static/media/Alan Font.0fd91999.woff"
+  },
+  {
+    "revision": "d0ead76c79aab05fcf13a37b41fc9107",
+    "url": "/static/media/pee-on-your-face.d0ead76c.woff"
+  }
+]);
